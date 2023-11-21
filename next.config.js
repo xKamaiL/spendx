1a4a3fb263487cdec8f3d3697bd9bac741daf52e
Pakorn Sangpeth <camel4882@gmail.com>
2023-11-21 21:51:55 +0700

    feat: add nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+module.exports = withPWA(nextConfig)
